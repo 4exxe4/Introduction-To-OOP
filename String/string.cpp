0ea049b36e72978b72d4a8e7c342e41f934c4835
user <user@WIN-CP209TC39O5>
2025-07-18 19:24:14 +0300

    Calling constructors DONE

--- String/string.cpp
+++ String/string.cpp
@@ -130,7 +130,7 @@ std::ostream& operator << (std::ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
-#define CONSTRUCTORS_CHECK
+//#define CONSTRUCTORS_CHECK
 //#define COPY_SEMANTIC_CHECK
 
 void main()
@@ -173,6 +173,48 @@ void main()
 	cout << str2 << endl;
 
 #endif // COPY_SEMANTIC_CHECK
+
+//MoveMethods (MoveSemantic)
+
+	String str1;           //DefaultConstructor
+	str1.print();
+
+	String str2(5);
+	str2.print();
+
+	String str3 = "Hello"; //Single-argument Constructor (const char str[])
+	str3.print();
+
+	String str4(); 
+	/*
+	В этой строке не вызывается DefaultConstructor, и не создается объект. 
+	Здесь объявляется функция 'str4', которая ничего не принимает, и возвращает значение типа 'String'.
+	То есть, круглые скобки не делают явный вызов конструктора по умолчанию.
+	Если есть необходимость явно вызвать конструктор по умолчанию, это можно сделать с помощью фигурных скобок
+	*/
+
+	String str5{};
+	/*
+	Эта строка уже явно вызывает DefaultConstructor()
+	*/
+	str5.print();
+
+	String str6{ 6 };     //Single-Argument Constructor (int)
+	str6.print();
+
+	String str7{ "World" };
+	str7.print();
+
+	String str8 = str7;
+	str8.print();
+
+	String str9(str8);
+	str9.print();
+
+	String str10{ str9 };
+	str10.print();
+
+	//Фигурные скобки для вызова конструкторов следует использовать с большой осторожностью
 }
 /*
 Использование динамической памяти в классе:
